287405e9b7968d53f83dda2f82d67c78833cc106
tripm3010 <tripm.uit@gmail.com>
2025-05-04 15:23:14 +0700

    Add Map/Dictionary-Based Strategy design pattern

--- post-service/src/main/java/com/tripm/postservice/dto/response/PostResponse.java
+++ post-service/src/main/java/com/tripm/postservice/dto/response/PostResponse.java
@@ -14,6 +14,7 @@ public class PostResponse {
     String id;
     String content;
     String userId;
+    String created;
     Instant createdDate;
     Instant modifiedDate;
 }
--- post-service/src/main/java/com/tripm/postservice/service/PostService.java
+++ post-service/src/main/java/com/tripm/postservice/service/PostService.java
@@ -25,6 +25,8 @@ import java.util.List;
 public class PostService {
     PostRepository postRepository;
     PostMapper postMapper;
+    DateTimeFormatter formatter;
+    private final DateTimeFormatter dateTimeFormatter;
 
     public PostResponse createPost(PostRequest request) {
 
@@ -51,12 +53,20 @@ public class PostService {
         Pageable pageable = PageRequest.of(page - 1, size, sort);
 
         var pageData = postRepository.findAllByUserId(userId, pageable);
+
+        var postList = pageData.getContent().stream().map(
+                post -> {
+                var postResponse = postMapper.toPostResponse(post);
+                postResponse.setCreated(dateTimeFormatter.format(post.getCreatedDate()));
+                return postResponse;
+                }).toList();
+
         return PageResponse.<PostResponse>builder()
                 .currentPage(page)
                 .pageSize(pageData.getTotalPages())
                 .totalPages(pageData.getTotalPages())
                 .totalElements(pageData.getTotalElements())
-                .data(pageData.stream().map(postMapper::toPostResponse).toList())
+                .data(postList)
                 .build();
     }
 
